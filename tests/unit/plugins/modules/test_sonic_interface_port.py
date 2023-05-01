d9b78101195c5c87e9fd783742d4769a4ca23275
Christian Svensson <blue@cmd.nu>
2023-05-01 16:54:54 +0200

    Add port speed and status

--- tests/unit/plugins/modules/test_sonic_interface_port.py
+++ tests/unit/plugins/modules/test_sonic_interface_port.py
@@ -12,6 +12,7 @@ from ansible_collections.community.sonic.plugins.modules import sonic_interface_
 PORT_TABLE = {
     'Ethernet0': {
         'alias': 'qsfp1',
+        'speed': '10000',
     }
 }
 
@@ -32,3 +33,33 @@ def test_clear_description_by_alias(value):
         PORT_TABLE, interface='qsfp1', description=value)
     assert changed, 'state was expected to change'
     assert 'description' not in new_state
+
+
+@pytest.mark.parametrize('value,expected', (
+    ('1G', '1000'),
+    ('100M', '100'),
+    ('400G', '400000')))
+def test_set_speed(value, expected):
+    _, new_state, _, changed = sonic_interface_port.mutate_state(
+        PORT_TABLE, interface='qsfp1', speed=value)
+    assert changed, 'state was expected to change'
+    assert new_state['speed'] == expected, f"got speed {new_state['speed']}"
+
+
+def test_set_invalid_speed():
+    try:
+        sonic_interface_port.mutate_state(PORT_TABLE, interface='qsfp1', speed='foobar')
+    except sonic_interface_port.InvalidSpeedError:
+        pass
+
+
+def test_set_enable_then_disable():
+    _, new_state, _, changed = sonic_interface_port.mutate_state(
+        PORT_TABLE, interface='qsfp1', enabled=True)
+    assert changed, 'state was expected to change'
+    assert new_state['admin_status'] == 'up', f"got admin_status {new_state['admin_status']}"
+    new_port_table = {'Ethernet0': new_state}
+    _, new_state, _, changed = sonic_interface_port.mutate_state(
+        new_port_table, interface='qsfp1', enabled=False)
+    assert changed, 'state was expected to change'
+    assert 'admin_status' not in new_state
